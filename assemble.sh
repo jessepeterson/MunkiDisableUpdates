d93c23c3632eb4313ceb4477dbbed2b286cfada6
Jesse Peterson <jesse@mactechs.com>
2014-07-15 14:32:06 -0700

    Make pkginfo

--- assemble.sh
+++ assemble.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+VERS=0.2
+NAME=AdobeFlashPlayerUpdateDisable
+
 OUTFILE=installcheck_script.py
 echo "Assembling $OUTFILE..."
 echo '#!/usr/bin/python' > $OUTFILE
@@ -17,3 +20,19 @@ cat xprotmeta.py >> $OUTFILE
 cat flashconfig.py >> $OUTFILE
 cat disable.py  >> $OUTFILE
 chmod 755 $OUTFILE
+
+echo "Creating $NAME-$VERS.pkginfo"
+# perhaps someday make this automatically update for AdobeFlashPlayer?
+#	--update_for=AdobeFlashPlayer \
+/usr/local/munki/makepkginfo \
+	--name=$NAME \
+	--displayname='Adobe Flash Player Update Disable' \
+	--description='Disables automatic update checking and updating for Adobe Flash.' \
+	--pkgvers=$VERS \
+	-c development -c testing \
+	--minimum_os_version=10.5.0 \
+	--nopkg \
+	--installcheck_script=installcheck_script.py \
+	--postinstall_script=postinstall_script.py \
+	--unattended_install \
+	> $NAME-$VERS.pkginfo
